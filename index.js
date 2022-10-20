e3cb6f28837b586a48efecbff80343ff9f1c86d6
Zachary Baldwin <zacharyjbaldwin@gmail.com>
2022-10-19 20:26:24 -0500

    updated

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const chalk = require('chalk');
+
 var gcd = function(a, b) {
     if (!b) {
         return a;
@@ -8,9 +10,21 @@ var gcd = function(a, b) {
 module.exports.calculateFrameSize = (jobs, hyperperiod) => {
     let executionTimes = [];
     let periods = [];
+    let deadlines = [];
+    let utilization = 0;
+    let utilizationRM = jobs.length * (Math.pow(2, (1/jobs.length)) - 1);
+    let simplyPeriodic = true;
+
     for (let i = 0; i < jobs.length; i++) {
         periods.push(jobs[i].p);
         executionTimes.push(jobs[i].e);
+        if (!jobs[i].d) { jobs[i].d = jobs[i].p; }
+        deadlines.push(jobs[i].d);
+        utilization += (jobs[i].e / Math.min(jobs[i].p, jobs[i].d));
+
+        for (let k = i + 1; k < jobs.length; k++) {
+            if (!(jobs[k].p % jobs[i].p == 0) || (jobs[i].p > jobs[i].d)) simplyPeriodic = false;
+        }
     }
     let maxExecutionTime = Math.max(...executionTimes);
     let dividesHyperiod = [];
@@ -31,13 +45,28 @@ module.exports.calculateFrameSize = (jobs, hyperperiod) => {
         console.log(`Job ${i+1}`);
         for (let k = 0; k < dividesHyperiod.length; k++) {
             let check = ((2*dividesHyperiod[k]) - gcd(jobs[i].p, dividesHyperiod[k]) <= jobs[i].d);
-            console.log(`2(${dividesHyperiod[k]})-gcd(${jobs[i].p}, ${dividesHyperiod[k]}) <= ${jobs[i].d} -> ${check ? 'YES' : 'NO'}`)
+            console.log(`2(${dividesHyperiod[k]})-gcd(${jobs[i].p}, ${dividesHyperiod[k]}) <= ${jobs[i].d} -> ${check ? chalk.green('YES') : chalk.red('NO')}`)
             if (check) { acceptedFrameSizes[i].push(dividesHyperiod[k]) }
         }
         console.log();
     }
 
     console.log('Accepted frame sizes:');
-    console.log(acceptedFrameSizes);
+
+    for (let i = 0; i < acceptedFrameSizes.length; i++) {
+        console.log(`Job ${i + 1}: {${chalk.yellow(acceptedFrameSizes[i].join(', '))}}`);
+    }
+
+    // console.log(acceptedFrameSizes);
+
+    console.log(`\nUtilization U = ${utilization}`);
+    console.log(`Utilization U_RM = ${utilizationRM}`);
+    let UrmGTU = utilizationRM >= utilization;
+    console.log(`U_RM >= U? -> ${UrmGTU ? chalk.green('YES') : chalk.red('NO')}`);
+    console.log(`Simply periodic? -> ${simplyPeriodic ? chalk.green('YES') : chalk.red('NO')}`);
+
+    console.log(`\nSurely schedulable with EDF? -> ${utilization <= 1 ? chalk.green('YES') : chalk.red('NO')}`);
+    console.log(`Surely schedulable with RM? -> ${UrmGTU || simplyPeriodic ? chalk.green('YES') : chalk.red('NO')}`);
+
     
 };
