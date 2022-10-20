2d3cb4a091a3991d9ba71406da25d4367945e5e7
Zachary Baldwin <zacharyjbaldwin@gmail.com>
2022-10-19 20:28:36 -0500

    testing conditions

--- test/test.js
+++ test/test.js
@@ -9,19 +9,13 @@ const calc = require('../index');
 // ];
 
 let jobs = [
-    { p: 25, e: 5, d: 25 },
-    { p: 50, e: 10, d: 50 },
-    { p: 100, e: 10, d: 100 },
-    { p: 200, e: 20, d: 200 },
-    { p: 200, e: 20, d: 200 },
+    { p: 25, e: 5 },
+    { p: 50, e: 10 },
+    { p: 100, e: 10 },
+    { p: 200, e: 20, d: 50 },
+    { p: 200, e: 20 },
 ];
 
-// let jobs = [
-//     { p: 25, e: 5 },
-//     { p: 50, e: 10 },
-//     { p: 100, e: 10},
-//     { p: 200, e: 20, d: 50 },
-//     { p: 200, e: 20, d: 50 }
-// ];
+let hyperperiod = 200;
 
-calc.calculateFrameSize(jobs, 200);
+calc.calculateFrameSize(jobs, hyperperiod);
